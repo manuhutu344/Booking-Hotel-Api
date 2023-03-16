ff1c2115517c3ffaad6aead4616b7869194c4832
manuhutu344 <nandoperdos2@gmail.com>
2023-03-16 19:48:59 +0900

    bcrypt password di db

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,14 +1,16 @@
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
 const User = require('../models/User')
+const bycryptSalt = bcrypt.genSaltSync(10)
 
-router.post('/register', (req, res)=>{
+router.post('/register', async(req, res)=>{
     const {name, email, password} = req.body
-    User.create({
+    const userDoc = await User.create({
         name, 
         email,
-        password,
+        password:bcrypt.hashSync(password, bycryptSalt),
     })
-    res.json({name, email, password})
+    res.json(userDoc)
 })
 
 module.exports = router

59c68b70bcbf6bdf75e614bc1a0f2c0ffe9d8345
manuhutu344 <nandoperdos2@gmail.com>
2023-03-16 18:38:31 +0900

    Memasukan Data Ke DB

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
+const User = require('../models/User')
 
 router.post('/register', (req, res)=>{
     const {name, email, password} = req.body
+    User.create({
+        name, 
+        email,
+        password,
+    })
     res.json({name, email, password})
 })
 

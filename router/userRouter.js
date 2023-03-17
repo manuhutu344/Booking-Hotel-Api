3ff37369f3010480b4e10c4499d26f9785bb0dda
manuhutu344 <nandoperdos2@gmail.com>
2023-03-17 20:12:34 +0900

    Membuat api register

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,12 +5,16 @@ const bycryptSalt = bcrypt.genSaltSync(10)
 
 router.post('/register', async(req, res)=>{
     const {name, email, password} = req.body
+    try {
     const userDoc = await User.create({
         name, 
         email,
         password:bcrypt.hashSync(password, bycryptSalt),
     })
     res.json(userDoc)
+} catch (e) {
+    res.status(422).json(e)
+}
 })
 
 module.exports = router

2203a581c2456b4e6bb4db7fc2be3cf4677117b7
manuhutu344 <nandoperdos2@gmail.com>
2023-03-20 18:25:56 +0900

    memperbaiki register

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,20 +1,20 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const User = require('../models/User')
-const bycryptSalt = bcrypt.genSaltSync(10)
+const bcryptSalt = bcrypt.genSaltSync(10)
 
 router.post('/register', async(req, res)=>{
     const {name, email, password} = req.body
     try {
-    const userDoc = await User.create({
-        name, 
-        email,
-        password:bcrypt.hashSync(password, bycryptSalt),
-    })
-    res.json(userDoc)
-} catch (error) {
-    res.status(422).json(error)
-}
+        const userDoc = await User.create({
+          name,
+          email,
+          password:bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc)
+      } catch (e) {
+        res.status(422).json(e)
+      }
 })
 
 router.post('/login', async (req, res)=>{

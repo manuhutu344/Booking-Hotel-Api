326544eb08d77fce6bdbf88de5991e495c8b2ab0
manuhutu344 <nandoperdos2@gmail.com>
2023-04-15 11:54:38 +0900

    Membuat backend mengupload gambar menggunakan link

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const userRouter = require('./router/userRouter')
 const CookieParser = require('cookie-parser')
+const postRouter = require('./router/postRouter')
 require('dotenv').config()
 require('./connection')
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(CookieParser())
 app.use(cors({credentials:true, origin:' http://localhost:5173'}))
 app.use('/user', userRouter)
+app.use('/post', postRouter)
 
 
 app.listen(4000, ()=>{

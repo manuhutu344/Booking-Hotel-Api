a44da2f4da00783c54520f9fa4b529631d1769a2
manuhutu344 <nandoperdos2@gmail.com>
2023-04-18 10:37:21 +0900

    Membuat gambar muncul tapi masih error

--- Server.js
+++ Server.js
@@ -1,17 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const imageDownloader = require('image-downloader')
 const userRouter = require('./router/userRouter')
 const CookieParser = require('cookie-parser')
-const postRouter = require('./router/postRouter')
 require('dotenv').config()
 require('./connection')
 const app = express()
 
 app.use(express.json())
 app.use(CookieParser())
+app.use('/uploads', express.static(__dirname+'/upload'))
 app.use(cors({credentials:true, origin:' http://localhost:5173'}))
 app.use('/user', userRouter)
-app.use('/post', postRouter)
+
+app.post('/upload-by-link', async(req, res)=>{
+    const {Link} = req.body
+    const newName =  'photo' + Date.now() + '.jpg'
+    await imageDownloader.image({
+        url: Link,
+        dest: __dirname + '/upload/' +newName
+    })
+    res.json(newName)
+})
 
 
 app.listen(4000, ()=>{

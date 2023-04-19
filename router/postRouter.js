8daed187a130f99a9fdea80feb07f304fe63cb6d
manuhutu344 <nandoperdos2@gmail.com>
2023-04-19 19:39:10 +0900

    Membuat untuk dapat mengambil gambar dari folder di komputer

--- router/postRouter.js
+++ router/postRouter.js
@@ -1,10 +1,19 @@
 const router = require('express').Router()
 const multer = require('multer')
+const fs = require('fs')
 
 const photosMiddleware = multer({dest:'upload/'})
 router.post('/upload', photosMiddleware.array('photos', 100),(req, res)=>{
-    console.log(req.files)
-    res.json(res.files)
+    const uploadedFiles = []
+    for(let i = 0; i < req.files.length; i++){
+        const {path, originalname} = req.files[i]
+        const parts = originalname.split('.')
+        const ext = parts[parts.length - 1]
+        const newPath = path + '.' + ext
+        fs.renameSync(path, newPath)
+        uploadedFiles.push(newPath.replace('upload',''))
+    }
+    res.json(uploadedFiles)
 })
 
 module.exports = router

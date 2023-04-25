f01264377743a950efa45d28e199738f9477b8b1
manuhutu344 <nandoperdos2@gmail.com>
2023-04-25 12:06:40 +0900

    Membuat Backend untuk memposting form dari FE

--- router/postRouter.js
+++ router/postRouter.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const multer = require('multer')
+const Place = require('../models/Place')
+const jwt = require('jsonwebtoken')
+const jwtSecret = '!@#$%^&*_-+=qwertyuiopasdfghjklzxcvbnm1234567890:;<>?'
 const fs = require('fs')
 
 const photosMiddleware = multer({dest:'upload/'})
@@ -14,6 +17,19 @@ router.post('/upload', photosMiddleware.array('photos', 100),(req, res)=>{
         uploadedFiles.push(newPath.replace('upload',''))
     }
     res.json(uploadedFiles)
+})
+
+router.post('/place', (req, res)=>{
+    const {token} = req.cookies
+    const{title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests} = req.body
+    jwt.verify(token, jwtSecret, {}, async(err, userData)=>{
+        if(err) throw err
+       const placeDoc = await Place.create({
+            owner: userData.id, 
+            title, address, addedPhotos, description, perks, extraInfo, checkIn, checkOut, maxGuests
+        })
+        res.json(placeDoc)
+    })
 })
 
 module.exports = router

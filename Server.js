8eec03868e820cf310f50f6a1f894da75adb116d
manuhutu344 <nandoperdos2@gmail.com>
2023-05-01 23:51:45 +0900

    Coba saja

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(CookieParser())
-app.use('/upload', express.static(__dirname+'/upload'))
+app.use('/upload', express.static(__dirname+'/upload/'))
 app.use(cors({credentials:true, origin:' http://localhost:5173'}))
 app.use('/user', userRouter)
 app.use('/post', postRouter)
